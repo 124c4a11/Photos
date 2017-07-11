5f94db625f3395657b5c7ff5f8c18e9fb786b48f
124c4a11 <124c4a11@mail.ru>
2017-07-11 15:44:05 +0300

    Sidebar.js: Added logic for .sidebar__user

--- src/js/modules/Sidebar.js
+++ src/js/modules/Sidebar.js
@@ -4,16 +4,23 @@ export default class Sidebar {
   constructor({container, toggle}) {
     this.$sidebar = $(container);
     this.$toggle = this.$sidebar.find(toggle);
+    this.$userInfo = this.$sidebar.find('.sidebar__user');
   }
 
   open() {
     this.$sidebar.addClass('sidebar_open');
     this.$toggle.addClass('hamburger_close');
+    if (this.$userInfo.length) {
+      this.$userInfo.removeClass('user_hidden');
+    }
   }
 
   close() {
     this.$sidebar.removeClass('sidebar_open');
     this.$toggle.removeClass('hamburger_close');
+    if (this.$userInfo.length) {
+      this.$userInfo.addClass('user_hidden');
+    }
   }
 
   clickOnToggle() {
