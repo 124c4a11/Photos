b02968ccc2eaee7e4c14592789db9bd3068a5c2c
124c4a11 <124c4a11@mail.ru>
2017-07-11 19:24:27 +0300

    Sidebar.js: Implemented logic for .sidebar__menu

--- src/js/modules/Sidebar.js
+++ src/js/modules/Sidebar.js
@@ -5,21 +5,32 @@ export default class Sidebar {
     this.$sidebar = $(container);
     this.$toggle = this.$sidebar.find(toggle);
     this.$userInfo = this.$sidebar.find('.sidebar__user');
+    this.$menu = this.$sidebar.find('.sidebar__menu');
   }
 
   open() {
     this.$sidebar.addClass('sidebar_open');
     this.$toggle.addClass('hamburger_close');
+
     if (this.$userInfo.length) {
       this.$userInfo.removeClass('user_hidden');
+    }
+
+    if (this.$menu.length) {
+      this.$menu.removeClass('main-menu_compact');
     }
   }
 
   close() {
     this.$sidebar.removeClass('sidebar_open');
     this.$toggle.removeClass('hamburger_close');
+
     if (this.$userInfo.length) {
       this.$userInfo.addClass('user_hidden');
+    }
+
+    if (this.$menu.length) {
+      this.$menu.addClass('main-menu_compact');
     }
   }
 
